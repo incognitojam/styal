78988cb5a5b97fc4cc93522c58af1d9b33df730b
Julius Marminge <julius0216@outlook.com>
2026-09-05 09:16:06 -0700

    refactor(ssh): keep package internals private (#10144)
    (cherry picked from commit 3fbc497b764ed904c4e50655da5b7a722d611fe5)
    Upstream-PR: 10144

--- packages/ssh/src/tunnel.ts
+++ packages/ssh/src/tunnel.ts
@@ -50,7 +50,7 @@ import {
   SshReadinessError,
 } from "./errors.ts";
 
-export const DEFAULT_REMOTE_PORT = 3773;
+const DEFAULT_REMOTE_PORT = 3773;
 const REMOTE_PORT_SCAN_WINDOW = 200;
 const SSH_READY_TIMEOUT_MS = 20_000;
 const SSH_READY_PROBE_TIMEOUT_MS = 1_000;
