dc4c96b12291092d126cca244260d8e0b0c289ad
Julius Marminge <julius0216@outlook.com>
2026-09-06 16:30:10 -0700

    fix(usage): read and redeem hub reset credits through CLIProxyAPI (#10395)
    (cherry picked from commit 0a89364f153f7ba890ae4dedd33d16675d73744f)
    Upstream-PR: 10395

--- packages/contracts/src/rpc.ts
+++ packages/contracts/src/rpc.ts
@@ -227,6 +227,7 @@ import {
   ResourceTelemetrySnapshot,
 } from "./resourceTelemetry.ts";
 import {
+  UsageLimitSourceError,
   ProviderConsumeResetCreditInput,
   ProviderConsumeResetCreditResult,
 } from "./providerUsageLimits.ts";
@@ -484,7 +485,7 @@ const ProviderSetupRpcError = Schema.Union([ProviderSetupError, EnvironmentAutho
 const WsProviderConsumeResetCreditRpc = Rpc.make(WS_METHODS.providerConsumeResetCredit, {
   payload: ProviderConsumeResetCreditInput,
   success: ProviderConsumeResetCreditResult,
-  error: ProviderSetupRpcError,
+  error: Schema.Union([ProviderSetupError, UsageLimitSourceError, EnvironmentAuthorizationError]),
 });
 
 const WsProviderAuthStartRpc = Rpc.make(WS_METHODS.providerAuthStart, {
