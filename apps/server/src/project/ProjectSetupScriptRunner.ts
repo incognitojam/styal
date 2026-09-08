3b9fc1083bfb095390ff04bf7eb5a39f4ce1138d
Julius Marminge <julius0216@outlook.com>
2026-09-08 01:11:09 -0700

    refactor(server): classify service exports (#10276)
    (cherry picked from commit 161715b1e4fa6d20fe3eb1cb9c91904bf83ca4c1)
    Upstream-PR: 10276

--- apps/server/src/project/ProjectSetupScriptRunner.ts
+++ apps/server/src/project/ProjectSetupScriptRunner.ts
@@ -173,6 +173,7 @@ export class ProjectSetupScriptRunner extends Context.Service<
   }
 >()("@styal/cli/project/ProjectSetupScriptRunner") {}
 
+/** @public Service construction is part of the canonical Effect module API. */
 export const make = Effect.gen(function* () {
   const projectionSnapshotQuery = yield* ProjectionSnapshotQuery.ProjectionSnapshotQuery;
   const terminalManager = yield* TerminalManager.TerminalManager;
