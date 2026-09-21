0bkevin/t3code

fix(sidebar): collapse settled and snoozed shelves by default (#9314)
Co-authored-by: maria-rcks <254055478+maria-rcks@users.noreply.github.com>

apps/web/src/components/Sidebar.tsx
@@ -211,9 +211,9 @@ import {
 // stays behind an explicit Show more.
 const SETTLED_TAIL_INITIAL_COUNT = 10;
 const SETTLED_TAIL_PAGE_COUNT = 25;
-// Keep the v2 key so existing preferences survive the v2-to-default rename.
-const SETTLED_SHELF_EXPANDED_KEY = "t3code:sidebar-v2:settled-expanded";
-const SNOOZED_SHELF_EXPANDED_KEY = "t3code:sidebar-v2:snoozed-expanded";
+// Fresh keys deliberately reset both shelves to collapsed for existing users.
+const SETTLED_SHELF_EXPANDED_KEY = "t3code:sidebar:settled-expanded";
+const SNOOZED_SHELF_EXPANDED_KEY = "t3code:sidebar:snoozed-expanded";
 
 function compactSidebarTimeLabel(label: string): string {
   if (label === "just now") return "now";
@@ -2274,7 +2274,7 @@ export default function Sidebar() {
   );
   const [settledShelfExpanded, setSettledShelfExpanded] = useLocalStorage(
     SETTLED_SHELF_EXPANDED_KEY,
-    true,
+    false,
     Schema.Boolean,
   );
   const toggleSettledShelf = useCallback(

incognitojam/styal

(cherry picked from commit 31eeb443305a4e11c8b20a7c8ff2b3f5e841eb0f)
Upstream-PR: 9314

apps/web/src/components/Sidebar.tsx
@@ -212,9 +212,9 @@ import {
 // stays behind an explicit Show more.
 const SETTLED_TAIL_INITIAL_COUNT = 10;
 const SETTLED_TAIL_PAGE_COUNT = 25;
-// The v2 suffix identifies the settled-shelf preference model.
-const SETTLED_SHELF_EXPANDED_KEY = "styal:sidebar-v2:settled-expanded";
-const SNOOZED_SHELF_EXPANDED_KEY = "styal:sidebar-v2:snoozed-expanded";
+// Fresh keys deliberately reset both shelves to collapsed for existing users.
+const SETTLED_SHELF_EXPANDED_KEY = "styal:sidebar:settled-expanded";
+const SNOOZED_SHELF_EXPANDED_KEY = "styal:sidebar:snoozed-expanded";
 
 function compactSidebarTimeLabel(label: string): string {
   if (label === "just now") return "now";
@@ -2301,7 +2301,7 @@ export default function Sidebar() {
   );
   const [settledShelfExpanded, setSettledShelfExpanded] = useLocalStorage(
     SETTLED_SHELF_EXPANDED_KEY,
-    true,
+    false,
     Schema.Boolean,
   );
   const toggleSettledShelf = useCallback(