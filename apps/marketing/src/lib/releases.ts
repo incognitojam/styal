Anurag38/t3code

feat(marketing): add a nightly channel to the download page (#10408)
Co-authored-by: Claude Fable 5.1 <noreply@anthropic.com>

apps/marketing/src/lib/releases.ts
@@ -1,9 +1,15 @@
 const REPO = "pingdotgg/t3code";
 
 export const RELEASES_URL = `https://github.com/${REPO}/releases`;
+export const NIGHTLY_RELEASES_URL = `${RELEASES_URL}?q=nightly&expanded=true`;
 
-const API_URL = `https://api.github.com/repos/${REPO}/releases/latest`;
-const CACHE_KEY = "t3code-latest-release";
+const LATEST_API_URL = `https://api.github.com/repos/${REPO}/releases/latest`;
+// The `latest` endpoint skips prereleases, so nightly needs the list. GitHub
+// returns it newest first and nightlies land several times a day, so the first
+// nightly tag in a small page is the current build.
+const LIST_API_URL = `https://api.github.com/repos/${REPO}/releases?per_page=10`;
+
+export type ReleaseChannel = "stable" | "nightly";
 
 export interface ReleaseAsset {
   name: string;
@@ -13,17 +19,36 @@ export interface ReleaseAsset {
 export interface Release {
   tag_name: string;
   html_url: string;
+  published_at: string;
   assets: ReleaseAsset[];
 }
 
-export async function fetchLatestRelease(): Promise<Release> {
-  const cached = sessionStorage.getItem(CACHE_KEY);
+function cacheKey(channel: ReleaseChannel) {
+  return `t3code-${channel}-release`;
+}
+
+async function fetchStable(): Promise<Release> {
+  return fetch(LATEST_API_URL).then((r) => r.json());
+}
+
+async function fetchNightly(): Promise<Release> {
+  const list: Release[] = await fetch(LIST_API_URL).then((r) => r.json());
+  const nightly = Array.isArray(list)
+    ? list.find((release) => release.tag_name?.includes("-nightly."))
+    : undefined;
+  if (!nightly) throw new Error("No nightly release in the latest page");
+  return nightly;
+}
+
+export async function fetchLatestRelease(channel: ReleaseChannel = "stable"): Promise<Release> {
+  const key = cacheKey(channel);
+  const cached = sessionStorage.getItem(key);
   if (cached) return JSON.parse(cached);
 
-  const data = await fetch(API_URL).then((r) => r.json());
+  const data = channel === "nightly" ? await fetchNightly() : await fetchStable();
 
   if (data?.assets) {
-    sessionStorage.setItem(CACHE_KEY, JSON.stringify(data));
+    sessionStorage.setItem(key, JSON.stringify(data));
   }
 
   return data;

incognitojam/styal

(cherry picked from commit 075a86e3b0152ea8f867ddd2c424739f5da08238)
Fork adaptation: release cache keys keep the fork's styal- prefix.
Upstream-PR: 10408

apps/marketing/src/lib/releases.ts
@@ -1,9 +1,15 @@
 const REPO = "pingdotgg/t3code";
 
 export const RELEASES_URL = `https://github.com/${REPO}/releases`;
+export const NIGHTLY_RELEASES_URL = `${RELEASES_URL}?q=nightly&expanded=true`;
 
-const API_URL = `https://api.github.com/repos/${REPO}/releases/latest`;
-const CACHE_KEY = "styal-latest-release";
+const LATEST_API_URL = `https://api.github.com/repos/${REPO}/releases/latest`;
+// The `latest` endpoint skips prereleases, so nightly needs the list. GitHub
+// returns it newest first and nightlies land several times a day, so the first
+// nightly tag in a small page is the current build.
+const LIST_API_URL = `https://api.github.com/repos/${REPO}/releases?per_page=10`;
+
+export type ReleaseChannel = "stable" | "nightly";
 
 export interface ReleaseAsset {
   name: string;
@@ -13,17 +19,36 @@ export interface ReleaseAsset {
 export interface Release {
   tag_name: string;
   html_url: string;
+  published_at: string;
   assets: ReleaseAsset[];
 }
 
-export async function fetchLatestRelease(): Promise<Release> {
-  const cached = sessionStorage.getItem(CACHE_KEY);
+function cacheKey(channel: ReleaseChannel) {
+  return `styal-${channel}-release`;
+}
+
+async function fetchStable(): Promise<Release> {
+  return fetch(LATEST_API_URL).then((r) => r.json());
+}
+
+async function fetchNightly(): Promise<Release> {
+  const list: Release[] = await fetch(LIST_API_URL).then((r) => r.json());
+  const nightly = Array.isArray(list)
+    ? list.find((release) => release.tag_name?.includes("-nightly."))
+    : undefined;
+  if (!nightly) throw new Error("No nightly release in the latest page");
+  return nightly;
+}
+
+export async function fetchLatestRelease(channel: ReleaseChannel = "stable"): Promise<Release> {
+  const key = cacheKey(channel);
+  const cached = sessionStorage.getItem(key);
   if (cached) return JSON.parse(cached);
 
-  const data = await fetch(API_URL).then((r) => r.json());
+  const data = channel === "nightly" ? await fetchNightly() : await fetchStable();
 
   if (data?.assets) {
-    sessionStorage.setItem(CACHE_KEY, JSON.stringify(data));
+    sessionStorage.setItem(key, JSON.stringify(data));
   }
 
   return data;