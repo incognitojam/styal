nico2525nn/t3code

feat(settings): add shared project defaults and scoped overrides (#9754)
Co-authored-by: maria-rcks <254055478+maria-rcks@users.noreply.github.com>

apps/server/src/project/ProjectSetupScriptRunner.test.ts
@@ -6,6 +6,7 @@ import * as Option from "effect/Option";
 import * as Schema from "effect/Schema";
 
 import * as ProjectionSnapshotQuery from "../orchestration/Services/ProjectionSnapshotQuery.ts";
+import * as ServerSettings from "../serverSettings.ts";
 import * as TerminalManager from "../terminal/Manager.ts";
 import * as ProjectSetupScriptRunner from "./ProjectSetupScriptRunner.ts";
 
@@ -68,13 +69,73 @@ const makeTerminalManagerLayer = (
 const testLayer = (
   project: OrchestrationProject,
   terminal: Pick<TerminalManager.TerminalManager["Service"], "open" | "write">,
+  settings = ServerSettings.layerTest(),
 ) =>
   ProjectSetupScriptRunner.layer.pipe(
     Layer.provideMerge(makeProjectionSnapshotQueryLayer(project)),
     Layer.provideMerge(makeTerminalManagerLayer(terminal)),
+    Layer.provide(settings),
   );
 
 describe("ProjectSetupScriptRunner", () => {
+  it.effect("runs the inherited machine setup action in the checkout's worktree", () => {
+    const open = vi.fn(() =>
+      Effect.succeed({
+        threadId: "thread-1",
+        terminalId: "setup-default-setup",
+        cwd: "/repo/worktrees/a",
+        worktreePath: "/repo/worktrees/a",
+        status: "running" as const,
+        pid: 123,
+        history: "",
+        exitCode: null,
+        exitSignal: null,
+        label: "setup-default-setup",
+        updatedAt: "2026-01-01T00:00:00.000Z",
+      }),
+    );
+    const write = vi.fn(() => Effect.void);
+    return Effect.gen(function* () {
+      const runner = yield* ProjectSetupScriptRunner.ProjectSetupScriptRunner;
+      const result = yield* runner.runForThread({
+        threadId: "thread-1",
+        projectId: "project-1",
+        worktreePath: "/repo/worktrees/a",
+      });
+      expect(result).toMatchObject({ status: "started", scriptId: "default-setup" });
+      expect(open).toHaveBeenCalledWith({
+        threadId: "thread-1",
+        terminalId: "setup-default-setup",
+        cwd: "/repo/worktrees/a",
+        worktreePath: "/repo/worktrees/a",
+        env: { T3CODE_PROJECT_ROOT: "/repo/project", T3CODE_WORKTREE_PATH: "/repo/worktrees/a" },
+      });
+      expect(write).toHaveBeenCalledWith({
+        threadId: "thread-1",
+        terminalId: "setup-default-setup",
+        data: "npm install\r",
+      });
+    }).pipe(
+      Effect.provide(
+        testLayer(
+          makeProject([]),
+          { open, write },
+          ServerSettings.layerTest({
+            defaultProjectScripts: [
+              {
+                id: "default-setup",
+                name: "Setup",
+                command: "npm install",
+                icon: "configure",
+                runOnWorktreeCreate: true,
+              },
+            ],
+          }),
+        ),
+      ),
+    );
+  });
+
   it.effect("returns no-script when no setup script exists", () => {
     const open = vi.fn(() => Effect.die("unexpected open"));
     const write = vi.fn(() => Effect.die("unexpected write"));

incognitojam/styal

(cherry picked from commit 9f40b2f563c662b43887b11ff99c466fe871c1af)
Fork adaptation: project settings keep the fork's additional instructions row, default repository choice, and review-before-import flow for project file actions, beside the new inherited defaults and browser access override. The fork's setup-script runner keeps its outcome activities and launches the resolved setup action with openCommand; the ported runner test covers an inherited machine action. The fork removed action preview URLs, so the shared actions list drops the preview badge and the chat view does not derive configured preview URLs. Onboarding keeps the fork's import step, which does not set a project model. The fork's welcome startup test gives its settings mock getSettings, which auto-bootstrap now reads.
Upstream-PR: 9754

apps/server/src/project/ProjectSetupScriptRunner.test.ts
@@ -16,6 +16,7 @@ import * as Stream from "effect/Stream";
 import * as OrchestrationEngine from "../orchestration/Services/OrchestrationEngine.ts";
 import * as ProjectionSnapshotQuery from "../orchestration/Services/ProjectionSnapshotQuery.ts";
 import * as ProjectionThreadActivities from "../persistence/Services/ProjectionThreadActivities.ts";
+import * as ServerSettings from "../serverSettings.ts";
 import * as TerminalManager from "../terminal/Manager.ts";
 import * as ProjectSetupScriptRunner from "./ProjectSetupScriptRunner.ts";
 
@@ -88,6 +89,7 @@ const testLayer = (
   terminal: Pick<TerminalManager.TerminalManager["Service"], "openCommand"> &
     Partial<Pick<TerminalManager.TerminalManager["Service"], "subscribe">>,
   commands: OrchestrationCommand[] = [],
+  settings = ServerSettings.layerTest(),
 ) =>
   ProjectSetupScriptRunner.layer.pipe(
     Layer.provideMerge(makeProjectionSnapshotQueryLayer(project)),
@@ -117,6 +119,7 @@ const testLayer = (
         deleteByThreadId: () => Effect.void,
       }),
     ),
+    Layer.provide(settings),
   );
 
 describe("ProjectSetupScriptRunner", () => {
@@ -167,6 +170,66 @@ describe("ProjectSetupScriptRunner", () => {
     ]);
     expect(runs[0]).not.toHaveProperty("scriptIcon");
     expect(runs[1]).not.toHaveProperty("scriptIcon");
+  });
+
+  it.effect("runs the inherited machine setup action in the checkout's worktree", () => {
+    const openCommand = vi.fn(() =>
+      Effect.succeed({
+        threadId: "thread-1",
+        terminalId: "setup-default-setup",
+        cwd: "/repo/worktrees/a",
+        worktreePath: "/repo/worktrees/a",
+        status: "running" as const,
+        pid: 123,
+        history: "",
+        exitCode: null,
+        exitSignal: null,
+        label: "setup-default-setup",
+        updatedAt: "2026-01-01T00:00:00.000Z",
+      }),
+    );
+    return Effect.gen(function* () {
+      const runner = yield* ProjectSetupScriptRunner.ProjectSetupScriptRunner;
+      const result = yield* runner.runForThread({
+        threadId: "thread-1",
+        projectId: "project-1",
+        worktreePath: "/repo/worktrees/a",
+      });
+      expect(result).toMatchObject({ status: "started", scriptId: "default-setup" });
+      expect(openCommand).toHaveBeenCalledWith({
+        threadId: "thread-1",
+        terminalId: "setup-default-setup",
+        cwd: "/repo/worktrees/a",
+        worktreePath: "/repo/worktrees/a",
+        env: {
+          STYAL_PROJECT_ROOT: "/repo/project",
+          STYAL_WORKTREE_PATH: "/repo/worktrees/a",
+          T3CODE_PROJECT_ROOT: "/repo/project",
+          T3CODE_WORKTREE_PATH: "/repo/worktrees/a",
+        },
+        command: "npm install",
+        label: "Setup",
+      });
+    }).pipe(
+      Effect.provide(
+        testLayer(
+          makeProject([]),
+          { openCommand },
+          [],
+          ServerSettings.layerTest({
+            defaultProjectScripts: [
+              {
+                id: "default-setup",
+                name: "Setup",
+                command: "npm install",
+                icon: "configure",
+                runOnWorktreeCreate: true,
+              },
+            ],
+          }),
+        ),
+      ),
+    );
   });
 
   it.effect("returns no-script when no setup script exists", () => {