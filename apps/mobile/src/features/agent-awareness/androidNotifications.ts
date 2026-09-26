nico2525nn/t3code

feat(mobile): add Android agent notifications and ongoing activity (#10416)
Co-authored-by: Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>

apps/mobile/src/features/agent-awareness/androidNotifications.ts
@@ -1,0 +1,35 @@
+import Constants from "expo-constants";
+import { requireOptionalNativeModule } from "expo";
+import { Platform } from "react-native";
+
+interface AndroidAgentNotifications {
+  configure(deviceId: string, userId: string, scheme: string, ongoingEnabled: boolean): void;
+  clear(): void;
+}
+
+const native =
+  Platform.OS === "android"
+    ? requireOptionalNativeModule<AndroidAgentNotifications>("T3AgentNotifications")
+    : null;
+
+export function supportsAndroidAgentNotifications(): boolean {
+  return typeof native?.configure === "function" && typeof native?.clear === "function";
+}
+
+export function configureAndroidAgentNotifications(
+  deviceId: string,
+  userId: string,
+  ongoingEnabled: boolean,
+): void {
+  const scheme = Constants.expoConfig?.scheme;
+  native?.configure?.(
+    deviceId,
+    userId,
+    (Array.isArray(scheme) ? scheme[0] : scheme) ?? "t3code",
+    ongoingEnabled,
+  );
+}
+
+export function clearAndroidAgentNotifications(): void {
+  native?.clear?.();
+}

incognitojam/styal

(cherry picked from commit 6c583620ff7ad3235b135af7107c0543467eecfa)
Fork adaptation: The relay keeps the fork's all-or-nothing APNs resolver (fork #265), so leaving every APNS_* value unset still runs it with push disabled; upstream's APNS_ENABLED switch is not needed. Upstream's FCM configuration, delivery queue, and consumer are added beside it, and the relay keeps its hourly cleanup cron. Mobile OTA updates stay tied to the fork's EAS project and also honor T3CODE_MOBILE_UPDATES_ENABLED=0. User-facing text, tests, Android scheme fallbacks, and the new docs use styal names, schemes, and build.styal.app package IDs.
Upstream-PR: 10416

apps/mobile/src/features/agent-awareness/androidNotifications.ts
@@ -1,0 +1,35 @@
+import Constants from "expo-constants";
+import { requireOptionalNativeModule } from "expo";
+import { Platform } from "react-native";
+
+interface AndroidAgentNotifications {
+  configure(deviceId: string, userId: string, scheme: string, ongoingEnabled: boolean): void;
+  clear(): void;
+}
+
+const native =
+  Platform.OS === "android"
+    ? requireOptionalNativeModule<AndroidAgentNotifications>("T3AgentNotifications")
+    : null;
+
+export function supportsAndroidAgentNotifications(): boolean {
+  return typeof native?.configure === "function" && typeof native?.clear === "function";
+}
+
+export function configureAndroidAgentNotifications(
+  deviceId: string,
+  userId: string,
+  ongoingEnabled: boolean,
+): void {
+  const scheme = Constants.expoConfig?.scheme;
+  native?.configure?.(
+    deviceId,
+    userId,
+    (Array.isArray(scheme) ? scheme[0] : scheme) ?? "styal",
+    ongoingEnabled,
+  );
+}
+
+export function clearAndroidAgentNotifications(): void {
+  native?.clear?.();
+}