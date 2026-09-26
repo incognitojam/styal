alcxyz/t3code

refactor(web): consolidate setup wizards into shared components (#10832)

apps/web/src/components/cloud/ConnectOnboardingDialog.tsx
@@ -15,18 +15,10 @@ import { useEnvironments, usePrimaryEnvironment } from "~/state/environments";
 import { CloudEnvironmentConnectRows } from "./CloudEnvironmentConnectList";
 import { Button } from "../ui/button";
 import { Checkbox } from "../ui/checkbox";
-import {
-  Dialog,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogPanel,
-  DialogPopup,
-  DialogTitle,
-} from "../ui/dialog";
+import { Dialog } from "../ui/dialog";
 import { Switch } from "../ui/switch";
 import { toastManager } from "../ui/toast";
-import { WizardSteps } from "../ui/wizard-steps";
+import { WizardSteps, WizardPopup, WizardHeader, WizardPanel, WizardFooter } from "../ui/wizard";
 
 /**
  * Post-sign-in onboarding wizard for T3 Connect. Opens on every in-session
@@ -216,23 +208,29 @@ function ConfiguredConnectOnboardingDialog() {
         if (!open && !isApplying) complete();
       }}
     >
-      <DialogPopup className="max-w-xl">
-        <DialogHeader>
-          <DialogTitle>Set up T3 Connect</DialogTitle>
-          <DialogDescription>
-            Mesh your devices together — publish this environment and connect the rest, all in one
-            place.
-          </DialogDescription>
+      <WizardPopup>
+        <WizardHeader
+          title="Set up T3 Connect"
+          description={
+            <>
+              Mesh your devices together — publish this environment and connect the rest, all in one
+              place.
+            </>
+          }
+        >
           {steps.length > 1 ? (
             <WizardSteps
-              steps={steps.map((id) => ({ id, label: STEP_LABELS[id] }))}
-              currentStep={step}
-              disabled={isApplying}
-              onStepSelect={setStep}
+              steps={steps.map((id) => STEP_LABELS[id])}
+              currentStep={steps.indexOf(step)}
+              isStepDisabled={() => isApplying}
+              onStepChange={(index) => {
+                const next = steps[index];
+                if (next) setStep(next);
+              }}
             />
           ) : null}
-        </DialogHeader>
-        <DialogPanel>
+        </WizardHeader>
+        <WizardPanel>
           {step === "publish" ? (
             <PublishStep
               exposeEnvironment={exposeEnvironment}
@@ -245,38 +243,37 @@ function ConfiguredConnectOnboardingDialog() {
           ) : (
             <DevicesStep />
           )}
-        </DialogPanel>
-        <DialogFooter variant="bare" className="sm:justify-between">
-          <label className="flex cursor-pointer items-center gap-2 self-start text-xs text-muted-foreground sm:self-center">
-            <Checkbox
-              checked={dontShowAgain}
-              onCheckedChange={(checked) => setDontShowAgain(checked === true)}
-            />
-            Don&apos;t show this again
-          </label>
-          <div className="flex flex-col-reverse gap-2 sm:flex-row">
-            {step === "publish" ? (
-              <>
-                <Button variant="ghost" disabled={isApplying} onClick={() => setStep("devices")}>
-                  Not now
-                </Button>
-                <Button
-                  disabled={
-                    isApplying || (controller.linkState.isPending && linkStateData === null)
-                  }
-                  onClick={() => void applyPublishSelection()}
-                >
-                  {isApplying ? "Enabling…" : "Continue"}
-                </Button>
-              </>
-            ) : (
-              <Button disabled={isApplying} onClick={complete}>
-                Done
+        </WizardPanel>
+        <WizardFooter
+          leading={
+            <label className="flex cursor-pointer items-center gap-2 self-start text-xs text-muted-foreground sm:self-center">
+              <Checkbox
+                checked={dontShowAgain}
+                onCheckedChange={(checked) => setDontShowAgain(checked === true)}
+              />
+              Don&apos;t show this again
+            </label>
+          }
+        >
+          {step === "publish" ? (
+            <>
+              <Button variant="ghost" disabled={isApplying} onClick={() => setStep("devices")}>
+                Not now
               </Button>
-            )}
-          </div>
-        </DialogFooter>
-      </DialogPopup>
+              <Button
+                disabled={isApplying || (controller.linkState.isPending && linkStateData === null)}
+                onClick={() => void applyPublishSelection()}
+              >
+                {isApplying ? "Enabling…" : "Continue"}
+              </Button>
+            </>
+          ) : (
+            <Button disabled={isApplying} onClick={complete}>
+              Done
+            </Button>
+          )}
+        </WizardFooter>
+      </WizardPopup>
     </Dialog>
   );
 }

incognitojam/styal

(cherry picked from commit 3faeee49ac67dfd9534369f1e1c627c0356b75ac)
Fork adaptation: The welcome wizard keeps the fork's own layout: its import and preferences steps (fork #478) need a flexible popup and panel, so WizardPanel keeps its className, div props, and animateHeight option alongside upstream's min-w-0 default. The styal Link onboarding dialog adopts the shared WizardPopup, WizardHeader, and WizardFooter components under its styal title.
Upstream-PR: 10832

apps/web/src/components/cloud/ConnectOnboardingDialog.tsx
@@ -15,18 +15,10 @@ import { useEnvironments, usePrimaryEnvironment } from "~/state/environments";
 import { CloudEnvironmentConnectRows } from "./CloudEnvironmentConnectList";
 import { Button } from "../ui/button";
 import { Checkbox } from "../ui/checkbox";
-import {
-  Dialog,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogPanel,
-  DialogPopup,
-  DialogTitle,
-} from "../ui/dialog";
+import { Dialog } from "../ui/dialog";
 import { Switch } from "../ui/switch";
 import { toastManager } from "../ui/toast";
-import { WizardSteps } from "../ui/wizard-steps";
+import { WizardSteps, WizardPopup, WizardHeader, WizardPanel, WizardFooter } from "../ui/wizard";
 
 /**
  * Post-sign-in onboarding wizard for styal Link. Opens on every in-session
@@ -216,23 +208,29 @@ function ConfiguredConnectOnboardingDialog() {
         if (!open && !isApplying) complete();
       }}
     >
-      <DialogPopup className="max-w-xl">
-        <DialogHeader>
-          <DialogTitle>Set up styal Link</DialogTitle>
-          <DialogDescription>
-            Mesh your devices together — publish this environment and connect the rest, all in one
-            place.
-          </DialogDescription>
+      <WizardPopup>
+        <WizardHeader
+          title="Set up styal Link"
+          description={
+            <>
+              Mesh your devices together — publish this environment and connect the rest, all in one
+              place.
+            </>
+          }
+        >
           {steps.length > 1 ? (
             <WizardSteps
-              steps={steps.map((id) => ({ id, label: STEP_LABELS[id] }))}
-              currentStep={step}
-              disabled={isApplying}
-              onStepSelect={setStep}
+              steps={steps.map((id) => STEP_LABELS[id])}
+              currentStep={steps.indexOf(step)}
+              isStepDisabled={() => isApplying}
+              onStepChange={(index) => {
+                const next = steps[index];
+                if (next) setStep(next);
+              }}
             />
           ) : null}
-        </DialogHeader>
-        <DialogPanel>
+        </WizardHeader>
+        <WizardPanel>
           {step === "publish" ? (
             <PublishStep
               exposeEnvironment={exposeEnvironment}
@@ -245,38 +243,37 @@ function ConfiguredConnectOnboardingDialog() {
           ) : (
             <DevicesStep />
           )}
-        </DialogPanel>
-        <DialogFooter variant="bare" className="sm:justify-between">
-          <label className="flex cursor-pointer items-center gap-2 self-start text-xs text-muted-foreground sm:self-center">
-            <Checkbox
-              checked={dontShowAgain}
-              onCheckedChange={(checked) => setDontShowAgain(checked === true)}
-            />
-            Don&apos;t show this again
-          </label>
-          <div className="flex flex-col-reverse gap-2 sm:flex-row">
-            {step === "publish" ? (
-              <>
-                <Button variant="ghost" disabled={isApplying} onClick={() => setStep("devices")}>
-                  Not now
-                </Button>
-                <Button
-                  disabled={
-                    isApplying || (controller.linkState.isPending && linkStateData === null)
-                  }
-                  onClick={() => void applyPublishSelection()}
-                >
-                  {isApplying ? "Enabling…" : "Continue"}
-                </Button>
-              </>
-            ) : (
-              <Button disabled={isApplying} onClick={complete}>
-                Done
+        </WizardPanel>
+        <WizardFooter
+          leading={
+            <label className="flex cursor-pointer items-center gap-2 self-start text-xs text-muted-foreground sm:self-center">
+              <Checkbox
+                checked={dontShowAgain}
+                onCheckedChange={(checked) => setDontShowAgain(checked === true)}
+              />
+              Don&apos;t show this again
+            </label>
+          }
+        >
+          {step === "publish" ? (
+            <>
+              <Button variant="ghost" disabled={isApplying} onClick={() => setStep("devices")}>
+                Not now
               </Button>
-            )}
-          </div>
-        </DialogFooter>
-      </DialogPopup>
+              <Button
+                disabled={isApplying || (controller.linkState.isPending && linkStateData === null)}
+                onClick={() => void applyPublishSelection()}
+              >
+                {isApplying ? "Enabling…" : "Continue"}
+              </Button>
+            </>
+          ) : (
+            <Button disabled={isApplying} onClick={complete}>
+              Done
+            </Button>
+          )}
+        </WizardFooter>
+      </WizardPopup>
     </Dialog>
   );
 }